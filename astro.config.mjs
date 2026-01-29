f98a9769791df4763b24702f94e1f5ed75fc4688
Mozart <dcdigitalpiano@gmail.com>
2026-01-29 16:16:56 +0900

    첫 배포 준비 완료

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: '디지털 피아노 갤러리 아카이브', // 사이드바 상단에 표시될 사이트 제목
+      sidebar: [
+        {
+          label: '카테고리 전체', // 메뉴 묶음의 대표 이름
+          autogenerate: { directory: '' }, // docs 폴더 내 모든 하위 폴더를 자동으로 메뉴화
+        },
+      ],
+    }),
+  ],
 });
